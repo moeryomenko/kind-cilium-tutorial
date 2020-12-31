cf3d2dfb622cb12d2469531f755020288c6352c0
Maxim Eryomenko <moeryomenko@gmail.com>
2020-12-31 09:40:57 +0300

    Add setup cluster script
    Signed-off-by: Maxim Eryomenko <moeryomenko@gmail.com>

---
+++ setup.zsh
@@ -0,0 +1,26 @@
+#!/bin/zsh
+
+# start kubernetes cluster with custom config.
+kind create cluster --config=kind-config.yaml
+kubectl cluster-info --context kind-kind
+
+# setup helm repository
+helm repo add cilium https://helm.cilium.io/
+
+# Preload the cilium image into each worker node in the kind cluster.
+docker pull cilium/cilium:v1.9.1
+kind load docker-image cilium/cilium:v1.9.1
+
+# install 
+helm install cilium cilium/cilium --version 1.9.1 \
+   --namespace kube-system \
+   --set nodeinit.enabled=true \
+   --set kubeProxyReplacement=partial \
+   --set hostServices.enabled=false \
+   --set externalIPs.enabled=true \
+   --set nodePort.enabled=true \
+   --set hostPort.enabled=true \
+   --set bpf.masquerade=false \
+   --set image.pullPolicy=IfNotPresent \
+   --set ipam.mode=kubernetes
+
